c83a2a2af90c0b1fffb00433bb1dcaa5d3870128
Mistry, Shefali <mistrys@corptst.anz.com>
2018-12-30 13:16:23 +1100

    gets the list of swift files that should be used to generate AST

--- lib/swift-ast-dump/swift_ast_dependencies_generator.rb
+++ lib/swift-ast-dump/swift_ast_dependencies_generator.rb
@@ -23,6 +23,32 @@ class SwiftAstDependenciesGenerator
     @tree
   end
 
+  #get the list of swift files 
+  def swift_files_list (folder_paths)
+
+    $stderr.puts "\n\n---------------swift_files_list---------------------"
+
+    swift_files_list = []
+
+    folder_paths.each { |path|
+      $stderr.puts "-------#{path}-----"
+      $stderr.puts "find #{path} -name *.swift"
+      IO.popen("find #{path} -name *.swift") { |f|
+        f.each do |line|
+          swift_files_list << line
+        end
+      }
+    }
+
+    swift_files_list.each { |file|
+      $stderr.puts "----file: #{file}"
+    }
+    $stderr.puts "\n\n\n\n"
+
+    return swift_files_list
+    
+  end
+
   #get list of paths which should be used to find swift source files
   def swift_files_path_list (swift_files_path, swift_ignore_folders)
     
