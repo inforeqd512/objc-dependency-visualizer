b1bd0aae999f755ad8ddeffb269e21fae6acc921
Mistry, Shefali <mistrys@corptst.anz.com>
2018-12-30 12:31:46 +1100

    path list to find swift files

--- lib/swift-ast-dump/swift_ast_dependencies_generator.rb
+++ lib/swift-ast-dump/swift_ast_dependencies_generator.rb
@@ -25,27 +25,35 @@ class SwiftAstDependenciesGenerator
 
   #get list of paths which should be used to find swift source files
   def swift_files_path_list (swift_files_path, swift_ignore_folders)
+    
+    $stderr.puts "\n\n----swift_files_path_list-----"
 
     paths = []
-
+    $stderr.puts "find #{swift_files_path} -type d -depth 1"
     IO.popen("find #{swift_files_path} -type d -depth 1") { |f|
       f.each do |line|
+        line.chomp!
         ignore_folder_match = false
         for ignore_folder in swift_ignore_folders do
-          if line.include?(ignore_folder)
+          if line.include?(ignore_folder) or line.include?("xcodeproj") #also exclude the xcodeproj file by default
             ignore_folder_match = true
             break
           end
         end
         
+        line_sub = line.gsub(/ /, '\ ')#global escape the spaces for folders that have several words
+
         if ignore_folder_match == false
-          paths << line
+          paths << line_sub
         end
       end
     }
+
     paths.each { |item|
       $stderr.puts "----#{item}"
     } 
+    $stderr.puts "\n\n\n\n"
+
     return paths
   end
 
