c8aacad2e8bd7058dc4ad3c1dd7414c8535fc767
Mistry, Shefali <mistrys@corptst.anz.com>
2019-01-01 17:52:20 +1100

    restore the swift ast files

--- lib/swift-ast-dump/swift_ast_dependencies_generator.rb
+++ lib/swift-ast-dump/swift_ast_dependencies_generator.rb
@@ -3,86 +3,24 @@ require 'swift-ast-dump/swift_ast_parser'
 
 class SwiftAstDependenciesGenerator
 
-  def initialize(swift_files_path, swift_ignore_folders, swift_ast_show_parsed_tree, verbose = false)
-    @swift_files_path = swift_files_path
-    @swift_ignore_folders = swift_ignore_folders
+  def initialize(ast_file, dump_parsed_tree, verbose = false)
+    @ast_file = ast_file
     @verbose = verbose
-    @dump_parsed_tree = swift_ast_show_parsed_tree
+    @dump_parsed_tree = dump_parsed_tree
   end
 
   # @return [DependencyTree]
   def generate_dependencies
 
     @tree = DependencyTree.new
+    @context = []
     @generics_context = []
 
-    folder_paths = swift_files_path_list(@swift_files_path, @swift_ignore_folders)
-    swift_files_list = swift_files_list(folder_paths)
-    # @ast_tree = SwiftAST::Parser.new().parse_build_log_output(File.read(@ast_file))
-    # @ast_tree.dump if @dump_parsed_tree
-    # scan_source_files
+    @ast_tree = SwiftAST::Parser.new().parse_build_log_output(File.read(@ast_file))
+    @ast_tree.dump if @dump_parsed_tree
+    scan_source_files
 
-    # @tree
-  end
-
-  #get the list of swift files 
-  def swift_files_list (folder_paths)
-
-    $stderr.puts "\n\n---------------swift_files_list---------------------"
-
-    swift_files_list = []
-
-    folder_paths.each { |path|
-      $stderr.puts "-------#{path}-----"
-      $stderr.puts "find #{path} -name *.swift"
-      IO.popen("find #{path} -name *.swift") { |f|
-        f.each do |line|
-          swift_files_list << line
-        end
-      }
-    }
-
-    swift_files_list.each { |file|
-      $stderr.puts "----file: #{file}"
-    }
-    $stderr.puts "\n\n\n\n"
-
-    return swift_files_list
-    
-  end
-
-  #get list of paths which should be used to find swift source files
-  def swift_files_path_list (swift_files_path, swift_ignore_folders)
-    
-    $stderr.puts "\n\n----swift_files_path_list-----"
-
-    paths = []
-    $stderr.puts "find #{swift_files_path} -type d -depth 1"
-    IO.popen("find #{swift_files_path} -type d -depth 1") { |f|
-      f.each do |line|
-        line.chomp!
-        ignore_folder_match = false
-        for ignore_folder in swift_ignore_folders do
-          if line.include?(ignore_folder) or line.include?("xcodeproj") #also exclude the xcodeproj file by default
-            ignore_folder_match = true
-            break
-          end
-        end
-        
-        line_sub = line.gsub(/ /, '\ ')#global escape the spaces for folders that have several words
-
-        if ignore_folder_match == false
-          paths << line_sub
-        end
-      end
-    }
-
-    paths.each { |item|
-      $stderr.puts "----#{item}"
-    } 
-    $stderr.puts "\n\n\n\n"
-
-    return paths
+    @tree
   end
 
   def scan_source_files
