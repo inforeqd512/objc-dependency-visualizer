96e1d0658cf6e72f486b7ee25938ead67cfba599
Mistry, Shefali <mistrys@corptst.anz.com>
2019-01-01 17:53:27 +1100

    extract common helpers

---
+++ lib/helpers/hierarchy_helpers.rb
@@ -0,0 +1,48 @@
+
+class Stack
+  def initialize
+    @data = []
+  end
+
+  def push a
+    @data.push a
+  end
+
+  def pop
+    @data.pop
+  end
+
+  def empty?
+    @data.empty?
+  end
+
+  def peek_last
+    return @data.last
+  end
+
+  def peek_tag_name index
+    if @data[index]
+      return @data[index].tag_name      
+    else
+      return "No Tag" #should not happen
+    end
+  end
+
+  def count
+    return @data.count
+  end
+end
+
+class DependencyHierarchyNode
+  attr_accessor :subclass, :superclass, :dependency
+
+  def initialize
+    @subclass = ""
+    @superclass = ""
+    @dependency = Set.new #unique entries for dependent classes
+  end
+
+  def add_dependency (dependent_node)
+    @dependency.add dependent_node    
+  end
+end
--- lib/objc/objc_dependencies_generator.rb
+++ lib/objc/objc_dependencies_generator.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'helpers/hierarchy_helpers'
 
 class ObjcDependenciesGenerator
 
@@ -236,40 +237,6 @@ class DwarfdumpHierarchyCreator
 
 end
 
-class Stack
-  def initialize
-    @data = []
-  end
-
-  def push a
-    @data.push a
-  end
-
-  def pop
-    @data.pop
-  end
-
-  def empty?
-    @data.empty?
-  end
-
-  def peek_last
-    return @data.last
-  end
-
-  def peek_tag_name index
-    if @data[index]
-      return @data[index].tag_name      
-    else
-      return "No Tag" #should not happen
-    end
-  end
-
-  def count
-    return @data.count
-  end
-end
-
 class TagHierarchyNode
   attr_reader :level_spaces_length, :tag_name
 
@@ -290,16 +257,3 @@ class TagHierarchyNode
 end
 
 
-class DependencyHierarchyNode
-  attr_accessor :subclass, :superclass, :dependency
-
-  def initialize
-    @subclass = ""
-    @superclass = ""
-    @dependency = Set.new #unique entries for dependent classes
-  end
-
-  def add_dependency (dependent_node)
-    @dependency.add dependent_node    
-  end
-end
