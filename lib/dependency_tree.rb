773f30d3fb0c573331e832767b9a80cc879ec3ca
Mistry, Shefali <mistrys@corptst.anz.com>
2019-01-12 23:25:45 +1100

    create new file for sigma js

--- lib/dependency_tree.rb
+++ lib/dependency_tree.rb
@@ -24,42 +24,21 @@ class DependencyTree
     @registry = {}
     @types_registry = {}
     @links_registry = {}
-    @nodes = {}
-    @edges = []
-    @id_generator = 0
   end
 
+  def add(source, dest, type = DependencyLinkType::UNKNOWN)
+    register source
+    register dest
+    register_link(source, dest, type)
 
+    return if connected?(source, dest)
 
+    @links_count += 1
+    @links += [{source: source, dest: dest}]
 
- 
-  #method to add source and dest details
+  end
+
   def add_new(source, dest, source_type = DependencyItemType::UNKNOWN, dest_type = DependencyItemType::UNKNOWN, link_type = DependencyItemType::UNKNOWN)
-
-    source_node = nil
-    if @nodes[source] == nil
-      node = TreeNode.new
-      node.label = source
-      @id_generator += 1
-      node.id = @id_generator
-
-      @nodes[source] = node
-      source_node = node
-      source_node = @nodes[source]
-    end
-
-    dest_node = nil
-    if @nodes[dest] == nil
-      node = TreeNode.new
-      node.label = dest
-      @id_generator += 1
-      node.id = @id_generator
-
-      @nodes[dest] = node
-      dest_node = node
-    else
-      dest_node = @nodes[dest]
-    end
 
     register(source, source_type)
     register(dest, dest_type)
@@ -67,50 +46,13 @@ class DependencyTree
 
     return if connected?(source, dest)
 
-    if source_node != nil and dest_node != nil
-      edge_node = nil
-      if connected?(source, dest) == false
-        edge = TreeEdge.new
-        edge.source = source_node.id
-        edge.target = dest_node.id
-        @id_generator += 1
-        edge.id = @id_generator
-
-        @edges.push(edge)
-        edge_node = node
-        source_node.num_links += 1
-      end
-    end
-
-
     @links_count += 1
     @links += [{source: source, dest: dest}]
-    
+
   end
 
   def connected?(source, dest)
     @links.any? {|item| item[:source] == source && item[:dest] == dest}
-  end
-
-  def nodes_array
-    sorted_nodes = @nodes.values.sort_by { |obj| obj.num_links }
-
-    total_cols = 50
-    scale = 30
-
-    total_nodes = sorted_nodes.count() 
-    for i in 0..(total_nodes - 1)
-      node = sorted_nodes[i]
-      node.x = (i % total_cols) * scale
-      node.y = ((i / total_cols).floor) * scale
-    end
-
-    sorted_nodes
-
-  end
-
-  def edges_array
-    return @edges
   end
 
   def isEmpty?
@@ -185,65 +127,6 @@ class DependencyTree
 
   def link_key(source, dest)
     source + '!<->!' + dest
-  end
-
-end
-
-
-class TreeNode
-
-  attr_accessor :label, :x, :y, :id, :color, :size, :num_links
-
-  def initialize
-    @label = ""
-    @x = 0.0
-    @y = 0.0
-    @id = 0
-    @color = '#36648B'
-    @size = 10
-    @num_links = 0
-  end
-
-  def as_json(options={})
-    {
-        label: @label,
-        x: @x,
-        y: @y,
-        id: @id,
-        color: @color,
-        size: @size,
-        num_links: @num_links
-
-    }
-  end
-
-  def to_json(*options)
-    as_json(*options).to_json(*options)
-  end
-
-end
-
-
-class TreeEdge
-
-  attr_accessor :source, :target, :id
-
-  def initialize
-    @source = 0
-    @target = 0
-    @id = 0
-  end
-
-  def as_json(options={})
-    {
-        source: @source,
-        target: @target,
-        id: @id
-    }
-  end
-
-  def to_json(*options)
-    as_json(*options).to_json(*options)
   end
 
 end
