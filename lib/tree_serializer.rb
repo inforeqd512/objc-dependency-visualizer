0cc3204eb2519669a14b7b598a7d99e8451568b7
Mistry, Shefali <mistrys@corptst.anz.com>
2019-01-15 13:22:39 +1100

    update swift primitives

--- lib/tree_serializer.rb
+++ lib/tree_serializer.rb
@@ -10,10 +10,12 @@ class TreeSerializer
   # @return [String]
   def serialize(output_format)
     object_to_serialize = {}
-    object_to_serialize[:edges] = @dependency_tree.edges_array
-    object_to_serialize[:nodes] = @dependency_tree.nodes_array
 
     case output_format
+    when 'd3js'
+      serialize_to_d3js(object_to_serialize)
+    when 'sigmajs'
+      serialize_to_sigmajs(object_to_serialize)
     when 'dot'
       serialize_to_dot(object_to_serialize)
     when 'json-pretty'
@@ -28,6 +30,23 @@ class TreeSerializer
       raise
     end
 
+  end
+
+  def serialize_to_d3js(object_to_serialize)
+    object_to_serialize[:links] = @dependency_tree.links_with_types
+    object_to_serialize[:links_count] = @dependency_tree.links_count
+    object_to_serialize[:objects] = Hash[
+      @dependency_tree.objects.map do |o|
+        [o, { type: @dependency_tree.type(o) }]
+      end
+    ]
+    serialize_to_json_var(object_to_serialize)
+  end
+
+  def serialize_to_sigmajs(object_to_serialize)
+    object_to_serialize[:edges] = @dependency_tree.edges_array
+    object_to_serialize[:nodes] = @dependency_tree.nodes_array
+    serialize_to_json(object_to_serialize)
   end
 
   def serialize_to_yaml(object_to_serialize)
