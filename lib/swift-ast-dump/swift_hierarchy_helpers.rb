975744fab37bda7b973b118ec77c4602e7341349
Mistry, Shefali <mistrys@corptst.anz.com>
2020-11-20 11:56:20 +1100

    log out swift file paths

--- lib/swift-ast-dump/swift_hierarchy_helpers.rb
+++ lib/swift-ast-dump/swift_hierarchy_helpers.rb
@@ -12,8 +12,15 @@ def find_swift_files(project_root_folder_path)
         paths << line
         end
     }
+
+    Logger.log_message("-----------paths swift count: #{paths.count}----------------------")
+    Logger.log_message("-----------paths swift: #{paths}----------------------")
+    Logger.log_message("\n\n\n\n")
+
+
     final_paths = final_paths(paths)
-    Logger.log_message("-----------final_paths: #{final_paths}----------------------")
+    Logger.log_message("-----------final_paths swift count: #{final_paths.count}----------------------")
+    Logger.log_message("-----------final_paths swift: #{final_paths}----------------------")
     Logger.log_message("\n\n\n\n")
 
     final_paths.sort! #just to match the objc logic for now
