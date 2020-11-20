abf2ca51d3fd75cf4060a3ed476cebc90786c36f
Mistry, Shefali <mistrys@corptst.anz.com>
2020-11-20 11:30:01 +1100

    ignore Plug in modules and Swift Interface files

--- lib/helpers/configuration.rb
+++ lib/helpers/configuration.rb
@@ -3,6 +3,8 @@ require 'set'
 class Configuration
   @@path_string_ignore = #exclude file paths to Tests in frameworks or subfolders, and Demo app
     Set.new([
+    'BinaryFramework',
+    '-Swift.h',
     'Content/',
     'Demo',
     'DerivedData',
